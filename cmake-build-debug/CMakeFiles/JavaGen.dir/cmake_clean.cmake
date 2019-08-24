7380d638cdcf19abfd8a23aeb1ae8fe4c3087b98
Zach Stoebner <zstoebner25@gmail.com>
2019-08-24 17:28:27 -0500

    First commit

---
+++ cmake-build-debug/CMakeFiles/JavaGen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JavaGen.dir/main.cpp.o"
+  "JavaGen.pdb"
+  "JavaGen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JavaGen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
